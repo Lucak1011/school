28fe120bd0aaffee32f0c8069f14c16f1802291b
Luca Kulaga <135963553+Lucak1011@users.noreply.github.com>
2023-07-04 15:07:42 +0100

    Add files via upload

---
+++ Task 1.py
@@ -0,0 +1,34 @@
+import pygame
+from pygame.locals import*
+pygame.init()
+
+window = pygame.display.set_mode((800,500))
+x = 0
+y = 0
+
+xpseed = 5
+yspeed = 1
+
+running = True
+while running == True:
+    pygame.time.delay(100)
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+        window.fill((255,0,0))
+        pygame.draw.rect(window, (120, 200, 255), (x, y, 20, 20))
+        x = x + xpseed
+        y = y + yspeed
+        if(x > 780):
+            xspeed = -xpseed
+
+
+        keys = pygame.key.get_pressed()
+        if keys[K_UP]:
+            print("HELLO!")
+
+            pygame.display.update()
+
+pygame.quit()
