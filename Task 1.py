30c9920138ea9d21c9dd4389d55ff0dc90b8592e
Luca Kulaga <135963553+Lucak1011@users.noreply.github.com>
2023-07-06 09:26:48 +0100

    Update Task 1.py

--- Task 1.py
+++ Task 1.py
@@ -2,12 +2,15 @@ import pygame
 from pygame.locals import*
 pygame.init()
 
+# /// DO NOT CHANGE THE BELOW \\\ #     
 window = pygame.display.set_mode((800,500))
 x = 0
 y = 0
 
 xpseed = 5
 yspeed = 1
+
+# /// DO NOT CHANGE THE ABOVE \\\ #
 
 running = True
 while running == True:
@@ -17,18 +20,21 @@ while running == True:
         if event.type == pygame.QUIT:
             running = False
 
-        window.fill((255,0,0))
-        pygame.draw.rect(window, (120, 200, 255), (x, y, 20, 20))
-        x = x + xpseed
-        y = y + yspeed
-        if(x > 780):
-            xspeed = -xpseed
+    window.fill((255,0,0))
+    pygame.draw.rect(window, (120, 200, 255), (x, y, 20, 20))
+    x = x + xpseed
+    y = y + yspeed
 
+    if(x > 780):
+        xspeed = -xpseed
+    if (x < 0):
+        xspeed = -xspeed
 
-        keys = pygame.key.get_pressed()
-        if keys[K_UP]:
-            print("HELLO!")
+# /// KEYBINDS \\\ #
+    keys = pygame.key.get_pressed()
+    if keys[K_UP]:
+        print("HELLO!")
 
-            pygame.display.update()
+    pygame.display.update()
 
 pygame.quit()
