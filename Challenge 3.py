bde63e5c06c4cb69123e5e10babeb1f73fb85a65
Luca Kulaga <135963553+Lucak1011@users.noreply.github.com>
2023-10-18 11:56:49 +0100

    Add files via upload

---
+++ Challenge 3.py
@@ -0,0 +1,23 @@
+print("Time to read a book calculator")
+
+pageadd = True
+
+while "pageadd" == True:
+    easy = 0
+    diff = 0
+    total = easy + diff
+    pagediff = input("Enter the difficulty of the page")
+    pagediff.lower()
+    if pagediff == "easy":
+        print("This page is easy")
+        easy = easy + 40
+        lp = input("Last page")
+        if lp == "No":
+            pageadd = True
+        elif lp == "Yes":
+            pageadd = False
+    elif pagediff == "Difficult":
+        print("This page is difficult")
+        diff = diff + 100
+
+print(total)
