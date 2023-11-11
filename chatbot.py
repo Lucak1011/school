472c5b517a7fbae16e0d3bd9896b71f5b05a6560
Lucak1011 <135963553+Lucak1011@users.noreply.github.com>
2023-11-11 22:40:52 +0000

    Added calculator function

--- chatbot.py
+++ chatbot.py
@@ -1,1 +1,18 @@
+print("Welcome to the chatbot")
 
+f = input("Enter a fucntion or enter 'function_list'")
+
+if f == "function_list":
+    print("1 = calculator, 2 = chat, 3 = joke")
+elif f == 1:
+    print("Calculator")
+    calc = True
+    while calc == True:
+        calcfun = input("What would you like to do?")
+
+        if calcfun == "add":
+            print("Enter two numbers")
+            n1 = int(input("Enter a number"))
+            n2 = int(input("Enter a number"))
+            sum = n1 + n2
+            print(sum)
