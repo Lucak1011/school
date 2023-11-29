bdc985c5410880ca5f210063e84399762eac126d
Lucak1011 <135963553+Lucak1011@users.noreply.github.com>
2023-11-29 18:38:35 +0000

    Added Q3

--- Luca/Adventuregame.py
+++ Luca/Adventuregame.py
@@ -3,14 +3,29 @@ def blade_light():
   print("Blade Light")
   print("Blade light is a low level enemy who should be fairly easy to defeat")
   print("Use your attacks wisely as these only regenerate each level")
-  global bl_health ==  100
+  global bl_health
+  bl_health = 100
 
-
+def pepsiman():
+    print("You meet pepsi man")
+    reply = input("Do you reply to Pepsi Man, Y or N")
+    if reply == "Y":
+        print("Pepsi Man gives you a magic key")
+        global magic_key
+        magic_key = True
+    else:
+        print("Pepsi man explodes and you die")
+def colonel_sanders():
+    print("Welcome to KFC Land lad")
+    print("Answer the next 3 questions correctly to proceed")
+    
 
 
 #/END CHARACTER FUNCTION\
 #Main Character Variables
-global mc_roundhousekick = 2
+global mc_roundhousekick
+
+mc_roundhousekick = 2
 
 
 
@@ -43,19 +58,30 @@ while blattack == True:
     if bl_health == 0:
       blattack = False
 print("Village 1 complete")
-print("You continue to the next Village")
-print("Welcome to village 2")
-print("Here you find that there is a little bit more of civilisation")
-print("You also can choose different paths here")
-choice1 = input("To your right, you continue a little deeper in to the village, or to the left, there is a forest. No matter which way you go there is always the possibility you run in to an enemy")
-if choice1 == right:
-  print("Welcome to the village market")
-  print("There is a enemy hiding somewhere, your challenge is to find them and defeat them")
-  print("You could either walk around and find them, or wait for them to find you")
-  choice2 = input("Enter 'wait' or 'walkaround'"):
-  if choice2 == "wait":
-    print("You wait around for a while and there is no sign of the enemy, you continue")
-  if choice2 == "walkaround":
-    print("You find the monster, he catches you by surprise and you have no time to attack, you die")
   
-
+#village 2
+print("Welcome to village two")
+path = input("You can go L or R")
+if path == "L":
+    pepsiman()
+if path == "R":
+    print("You go right")
+    colonel_sanders()
+    q1 = input("What is KFC's slogan")
+    q1.lower()
+    if q1 == "finger lickin good":
+        print("Correct")
+        q2 = input("Who is Colonel sanders")
+        if q2 == "KFC Founder":
+           q2.lower()
+           print("Success - continue")
+           q3 = input("Rayhan - Y or N")
+           q3.lower()
+           if q3 == "Y":
+              print("Success")
+           else:
+              print("Wrong. Bye bye")
+        else:
+           print("Wrong. Bye bye")
+    else:
+        print("Wrong. Bye bye")
